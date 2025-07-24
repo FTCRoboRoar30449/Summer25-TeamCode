36d27c4107b044a11a3371522171f8b6320a734f
TanviDsouza <tanvisdsouza@gmail.com>
2025-07-24 00:15:36 -0700

    Update AutoBlue.java
    added coordinates for PedroPathing and defined poses

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoBlue.java
@@ -0,0 +1,27 @@
+package pedroPathing.examples;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.localization.Pose;
+import com.pedropathing.pathgen.BezierCurve;
+import com.pedropathing.pathgen.BezierLine;
+import com.pedropathing.pathgen.Path;
+import com.pedropathing.pathgen.PathChain;
+import com.pedropathing.pathgen.Point;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import pedroPathing.constants.FConstants;
+import pedroPathing.constants.LConstants;
+
+private final Pose startPose = new Pose(8, 80, Math.toRadians(0));  // Starting/idle position
+private final Pose dropPose = new Pose(20, 120, Math.toRadians(-45)); // Scoring/high basket position
+
+private final Pose preStaged1Pose = new Pose(30, 120, Math.toRadians(0)); // First Pre-staged sample pickup
+private final Pose preStaged2Pose = new Pose(30, 130, Math.toRadians(0)); // Second Pre-staged sample pickup
+private final Pose preStaged3Pose = new Pose(30, 130, Math.toRadians(45)); // Third Pre-staged sample pickup
+
+private final Pose subPose = new Pose(71, 105, Math.toRadians(-90)); // sub pickup/position
+// **dont get heading** //private final Pose subControlPose = new Pose(60, 98, Math.toRadians(90)); // Control point for curved path to sub position
+private final Pose endPose = new Pose(70, 100, Math.toRadians(90));    // Endgame position - low rung
+
